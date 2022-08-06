8d432b3edd7b8743cd273889dfa609a6bcd72d72
Barry Jackson <43217434+btjackson-usgs@users.noreply.github.com>
2022-08-06 09:54:39 -0500

    Create super_simple_collect.php
    Simply version no external css or jscript references (beyond what's need to talk to the blockchain). Make responsive also.

---
+++ super_simple_collect.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    
+    
+  </head>  
+  <body>
+    
+    <form name=form1>
+      <table><tr>
+        <td>Renter Name</td>
+        <td>
+      <input type=text name="renter_name">
+        </td></tr>
+        
+      <tr>
+        <td>Address Name</td>
+        <td>
+      <input type=text name="renter_name" required>
+        </td></tr>
+          
+        <tr>
+        <td>Zip code</td>
+        <td>
+      <input type=text name="zip" required>
+        </td></tr>
+                   
+        <tr><td colspan=2>
+          <input type=submit value="Pay Rent">
+          </td></tr>
+      </table>
